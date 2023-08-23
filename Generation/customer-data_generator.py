52dc3bf7cd9d901891c1e7c4c2f5d34c2a599919
SHANIQUE <81657345+shanshanx14@users.noreply.github.com>
2023-08-23 12:11:22 -0500

    script to generate journaled customer data

--- Generation/customer-data_generator.py
+++ Generation/customer-data_generator.py
@@ -1,11 +1,11 @@
-import json, os, random
+import json, os, random, multiprocessing
 from datetime import date
 from faker import Faker
 
 today = date.today()  # get today's date
-path = f"../Kafka/{today}"  # creates subfolder for journaled data per day
+path = f"../Kafka_Customers/{today}"  # creates subfolder for journaled data per day
 
-customer = {"id": "",  # customer journaled data
+customer = {"id": "",  # customer journaled data schema
             "active": "",
             "subscription": "",
             "customer_first_name": "",
@@ -21,7 +21,7 @@ def get_name(number: int) -> str:  # creates a numbered json filename with lates
     return name
 
 
-def get_info(journal: dict) -> dict:
+def get_info(journal: dict) -> dict: # creates data for the journals
     fake = Faker()
     types = ["Basic", "Standard", "Family Package", "Premium", "Xtreme Premium",
              "Supreme", "Pay-As-Go", "Fixed Access", "Enterprise", "Supreme Plus"]
@@ -50,7 +50,7 @@ def get_info(journal: dict) -> dict:
     return journal
 
 
-def generate():
+def generate(): # stores generated data as json file
     for n in range(1, 101):
         file = get_name(n)
         data = get_info(customer)
@@ -59,10 +59,13 @@ def generate():
 
 
 if __name__ == '__main__':
-    if not os.path.exists(path):
-        os.makedirs(path)
-        generate()
 
-    else:
+    if not os.path.exists(path):  # checks if path does not exist before generation begins
+        os.makedirs(path)  # creates path where data will be stored
+        process = multiprocessing.Process(target=generate)  # uses process pool to handle generation
+        process.start()  # process starts
+        process.join()  # waits for process to end
+
+    else:  # when path exists, no data is generated and program exists
         print("Path already exists and is a directory!")
         exit(1)
